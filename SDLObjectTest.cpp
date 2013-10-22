f67ed8091d3d39ae30b71665f14ebaeec91d6ae0
Alex <Amarcolina@gmail.com>
2013-10-21 21:25:20 -0700

    Updated project to use SDL2.0

--- SDLObjectTest.cpp
+++ SDLObjectTest.cpp
@@ -6,13 +6,12 @@
  */
 
 #include "SDL/SDL.h"
-//#include "SDL/SDL_ttf.h"
 #include "Timer.h"
 #include "Dot.h"
 #include <string>
 
-const int SCREEN_WIDTH = 762;
-const int SCREEN_HEIGHT = 441;
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 const int SCREEN_BPP = 32;
 const int FRAMES_PER_SECOND = 20;
 //const int DOT_WIDTH = 20;
@@ -21,18 +20,14 @@ const int FRAMES_PER_SECOND = 20;
 
 bool init();
 void clean_up();
-SDL_Surface *load_image(std::string filename);
-void apply_surface(int x, int y, SDL_Surface* source, SDL_Surface* destination);
+SDL_Texture *load_texture(SDL_Renderer* renderer, std::string filename);
+void draw_texture(SDL_Renderer* renderer, SDL_Texture* texture, int x, int y, int width, int height);
 
 int main(int argc, char* args[]) {
 	bool quit = false;
 	int frame = 0;
 	bool capFPS = true;
 	Timer fps;
-	// the images
-	SDL_Surface *screen = NULL;
-	SDL_Surface *dot = NULL;
-	SDL_Surface *background = NULL;
 	Dot myDot;
 
 	// set up window
@@ -40,25 +35,27 @@ int main(int argc, char* args[]) {
 	if (!init()) {
 		return 1;
 	}
-	//setup screen
-	screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP,
-			SDL_SWSURFACE);
-	if (screen == NULL) {
+
+	//setup window
+	SDL_Window *window = SDL_CreateWindow("Test Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_Texture *screen = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	//apply_surface(renderer, background, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+
+	if (window == NULL) {
 		return 1;
 	}
-	//load the images
-	dot = SDL_LoadBMP("greenPixelMan.bmp");
-	background = load_image("floorplan_Zoomed.bmp");
 
-	//Apply the background to the screen
-	apply_surface(0, 0, background, screen);
-	//apply the message to the screen
-	//apply_surface( 50, 50, message, screen);
+	//Create the textures
+	SDL_Texture* dot = load_texture(renderer, "greenPixelMan.bmp");
+	SDL_Texture* background = load_texture(renderer, "floorplan_Zoomed.bmp");
 
 	//Event code
 	SDL_Event event;
-
-
 
 	//while the user hasn't quit
 	while (quit == false) {
@@ -77,72 +74,60 @@ int main(int argc, char* args[]) {
 		myDot.move();
 	// Draw function stuff
 		//Fill in the background
-		apply_surface(0, 0, background, screen);
+		draw_texture(renderer, background, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 		//Then show the Dot
-		//myDot.show();
-		apply_surface( myDot.x, myDot.y, dot, screen );
+		draw_texture(renderer, dot, myDot.x, myDot.y, 50, 50);
 		//Update Screen
-		if (SDL_Flip(screen) == -1) {
-			return 1;
-		}
+		SDL_RenderPresent(renderer);
+
 		//Cap the frame rate
 		if(fps.get_ticks() < 1000 / FRAMES_PER_SECOND){
 			SDL_Delay( (1000/FRAMES_PER_SECOND) - fps.get_ticks() );
 		}
-
 	}
 
 	//Free the loaded image
-	SDL_FreeSurface(dot);
-	SDL_FreeSurface(background);
+	SDL_DestroyTexture(dot);
+	SDL_DestroyTexture(background);
+	SDL_DestroyRenderer(renderer);
 	clean_up();
 
 	return 0;
 }
-bool init() {
 
+bool init() {
 	//Start SDL
 	if (SDL_Init(SDL_INIT_EVERYTHING) == -1) {
-		return 1;
+		return false;
 	}
-
-	//set the window caption
-	SDL_WM_SetCaption("SDL Object Test", NULL);
-
 	return true;
 }
+
 void clean_up() {
 	//Quit SDL
 	SDL_Quit();
 }
 
-SDL_Surface *load_image(std::string filename) {
-	//temporary storage for the image that's loaded
-	SDL_Surface* loadedImage = NULL;
-	//the optimized image that will be used
-	SDL_Surface* optimizedImage = NULL;
-
-	//load using SDL image
-	loadedImage = SDL_LoadBMP( filename.c_str() );
-
-	//if nothing went wrong in loading the image
-	if (loadedImage != NULL) {
-		//create and optimized image
-		optimizedImage = SDL_DisplayFormat(loadedImage);
-		//free the old image
-		SDL_FreeSurface(loadedImage);
+SDL_Texture *load_texture(SDL_Renderer* renderer, std::string filename) {
+	//Load image into a Surface first, then load it into a Texture
+	SDL_Surface* loadedImage = SDL_LoadBMP(filename.c_str());
+	if (loadedImage == NULL){
+		return NULL;
 	}
-	// return the optimized image
-	return optimizedImage;
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, loadedImage);
+	SDL_FreeSurface(loadedImage);
+	return texture;
 }
-void apply_surface(int x, int y, SDL_Surface* source,
-		SDL_Surface* destination) {
+
+void draw_texture(SDL_Renderer* renderer, SDL_Texture* texture, int x, int y, int width, int height) {
 	//Make a temp rectangle to hold the offsets
 	SDL_Rect offset;
 	//Give the offsets to the rectangle
 	offset.x = x;
 	offset.y = y;
+	offset.w = width;
+	offset.h = height;
 	//Blit the surface
-	SDL_BlitSurface(source, NULL, destination, &offset);
+	SDL_RenderCopy(renderer, texture, NULL, &offset);
 }
 
